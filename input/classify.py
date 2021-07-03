b0c5734f18cbecf39ad2600d3443ae81c8699f34
hyao <s2132237@gmail.com>
2021-07-03 16:59:39 +0800

    update workflow of protein classify

---
+++ input/classify.py
@@ -0,0 +1,42 @@
+import pickle
+
+def LCP(strs):
+    if not strs:
+        return ''
+    for i, letter_g in enumerate(zip(*strs)):
+        if len(set(letter_g)) > 1:
+            return strs[0][:i]
+    else:
+        return min(strs)
+#print(LCP(['1','aa1aaaa']))
+
+
+with open("subgraph.txt", 'r') as f:
+    max_len = 0
+    for line in f.readlines():
+        input_code = line.split()[1]
+        if len(input_code) > max_len:
+            max_code = input_code
+            max_len = len(input_code)
+
+#print(max_code)
+
+with open('lcs_all_class_dict', 'rb') as f:
+    dic = pickle.load(f)
+
+#print(dic.items())
+
+outclass = ''
+longest_len = 0
+longest_common_code = ''
+for class_name in dic:
+    for target_code in dic[class_name]:
+        string = LCP([max_code, target_code])
+        if len(string) > 0:
+            if len(string) > longest_len:
+                #print(class_name, 
+                longest_len = len(string)
+                outclass = class_name
+                longest_common_code = string
+print(outclass)
+print(longest_common_code)
---
+++ input/workflow.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#echo "argv:$1"
+
+
+./P1  < $1 | grep 'Code' > 'subgraph.txt'
+python3 classify.py 
